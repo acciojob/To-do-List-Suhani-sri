ffdac409ea30d85301b2d7b91c9a055fd6ec24e2
Suhani-sri <122045114+Suhani-sri@users.noreply.github.com>
2023-10-01 12:05:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let clickBtn = document.getElementById("addTodoBtn");
 clickBtn.addEventListener("click",addItem);
 function addItem(){
-	let listItem = document.createElement('li');
+	
 	let item = document.getElementById("newTodoInput");
-	listItem.innerText=item.value;
-	let olist = document.getElementById("todoList");
-	olist.append(listItem);
+	const newTodo = item.value.trim();
+	if(newTodo!==''){
+		let listItem = document.createElement('li');
+		listItem.innerText=newTodo;
+		let olist = document.getElementById("todoList");
+		olist.append(listItem);
+		item.value = '';
+	}
+	
 }
 
