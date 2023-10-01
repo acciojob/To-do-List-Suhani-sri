0ef9dd9057d5a7465c6c57cc6659557d23bb7647
Suhani-sri <122045114+Suhani-sri@users.noreply.github.com>
2023-10-01 11:48:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-//your code here
+let clickBtn = document.getElementById("addTodoBtn");
+clickBtn.addEventListner("click",addItem);
+function addItem(){
+	let listItem = document.createElement('li');
+	let item = document.getElementById("newTodoInput");
+	listItem.innerText(item);
+	let olist = document.getElementById("todoList");
+	olist.append(listItem);
+}
 
